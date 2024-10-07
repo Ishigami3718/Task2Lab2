6378e8cc45214e3438e73f6df7bf931f279d3080
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2024-10-07 10:09:30 +0300

    constructor and ToString

--- MyFrac_Task2Lab2/Program.cs
+++ MyFrac_Task2Lab2/Program.cs
@@ -3,6 +3,30 @@ namespace Lab
 {
     class MyFrac
     {
+        private long nom, denom;
+
+        private long Evklid(long f,long s)
+        {
+            while (s!=0)
+            {
+                long temp = s;
+                s = f % s;
+                f = temp;
+            }
+            return f;
+        }
+        public MyFrac(long nom, long denom)
+        {
+            long nsd = Evklid(nom, denom);
+            this.nom = nom/nsd;
+            this.denom = denom/nsd;
+        }
+
+        public override string ToString()
+        {
+            return $"{nom}/{denom}";
+        }
+
 
     }
 
@@ -10,7 +34,7 @@ namespace Lab
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
